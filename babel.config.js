fce87b3686c5b0b09c68b918dc16a35312912168
Robert Fischer <robert@beewell>
2018-09-04 12:55:00 -0400

    Use the lodash Babel plugin for modular builds

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
   api.cache(() => process.env.NODE_ENV);
 
   const presets = [ "@babel/flow", "minify" ];
-  const plugins = [ ];
+  const plugins = [ "lodash" ];
 
   return {
     presets,
